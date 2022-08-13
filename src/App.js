753156b70a8861516d02585951ab054d7b0ed289
sksami3 <sksami4456@gmail.com>
2022-08-14 00:12:43 +0600

    Menu component ongoing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
-import {Navbar, NavbarBrand} from 'reactstrap';
+import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes'
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div>
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+  render() {
+    return (
+      <div>
         <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/">Developer's Restaurant</NavbarBrand>
           </div>
         </Navbar>
-        <Menu />
+        <Menu dishes={this.state.dishes} />
       </div>
-  );
+    );
+  }
 }
 
 export default App;
