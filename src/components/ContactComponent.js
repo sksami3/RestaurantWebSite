5bb819a97fbbba3e361a2358c7d70cfcb36dafd4
sksami3 <sksami4456@gmail.com>
2022-10-03 00:15:39 +0600

    validators done

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -5,6 +5,12 @@ import {
 } from 'reactstrap';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !(val) || (val.length <= len);
+const minLength = (len) => (val) => val && (val.length >= len);
+const isNumber = (val) => !isNaN(Number(val));
+const validEmail = (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
+
 class Contact extends Component {
 
     constructor(props) {
@@ -63,8 +69,21 @@ class Contact extends Component {
                                 <Control.text model=".firstname" id="firstname" name="firstname"
                                     placeholder="First Name"
                                     className="form-control"
+                                    validators={{
+                                        required, minLength: minLength(3), maxLength: maxLength(15)
+                                    }}
                                 />
                             </Col>
+                            <Errors
+                                className="text-danger"
+                                model=".firstname"
+                                show="touched"
+                                messages={{
+                                    required: 'Required',
+                                    minLength: 'Must be greater than 2 characters',
+                                    maxLength: 'Must be 15 characters or less'
+                                }}
+                            />
                         </Row>
                         <Row className="form-group">
                             <Label htmlFor="lastname" md={2}>Last Name</Label>
